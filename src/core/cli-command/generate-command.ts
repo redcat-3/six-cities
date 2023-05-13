618e9e7f9349be6bcf2a3baccc786fd8475d0d37
redcat <redcat-3@yandex.ru>
2023-05-14 01:47:55 +0800

    add command --generate and refactoring --import

---
+++ src/core/cli-command/generate-command.ts
@@ -0,0 +1,27 @@
+import got from 'got';
+import { CliCommandInterface } from './cli-command.interface.js';
+import { MockData } from '../../types/mock-data.type.js';
+import OfferGenerator from '../../modules/offer-generator/offer-generator.js';
+import TSVFileWriter from '../file-writer/tsv-file-writer.js';
+
+export default class GenerateCommand implements CliCommandInterface {
+  public readonly name = '--generate';
+  private initialData!: MockData;
+
+  public async execute(...parameters:string[]): Promise<void> {
+    const [count, filepath, url] = parameters;
+    const offerCount = Number.parseInt(count, 10);
+    try {
+      this.initialData = await got.get(url).json();
+    } catch {
+      console.log(`Can't fetch data from ${url}.`);
+      return;
+    }
+    const offerGeneratorString = new OfferGenerator(this.initialData);
+    const tsvFileWriter = new TSVFileWriter(filepath);
+    for (let i = 0; i < offerCount; i++) {
+      await tsvFileWriter.write(offerGeneratorString.generate());
+    }
+    console.log(`File ${filepath} was created!`);
+  }
+}
