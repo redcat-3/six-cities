8189255f3be21feb144ec37e0d503cfa88641bc4
redcat <redcat-3@yandex.ru>
2023-06-07 16:37:43 +0800

    add Offer controller

---
+++ src/modules/offer/offer.controller.ts
@@ -0,0 +1,37 @@
+import { Request, Response } from 'express';
+import { inject, injectable } from 'inversify';
+import { Controller } from '../../core/controller/controller.abstract.js';
+import { LoggerInterface } from '../../core/logger/logger.interface.js';
+import { AppComponent } from '../../types/app-component.enum.js';
+import { HttpMethod } from '../../types/http-method.enum.js';
+import { OfferServiceInterface } from './offer-service.interface.js';
+import { fillDTO } from '../../core/helpers/index.js';
+import OfferRdo from './rdo/offer.rdo.js';
+import CreateOfferDto from './dto/create-offer.dto.js';
+
+@injectable()
+export default class OfferController extends Controller {
+  constructor(
+    @inject(AppComponent.LoggerInterface) protected readonly logger: LoggerInterface,
+    @inject(AppComponent.OfferServiceInterface) private readonly offerService: OfferServiceInterface
+  ) {
+    super(logger);
+    this.logger.info('Register routes for OfferController…');
+
+    this.addRoute({ path: '/', method: HttpMethod.Get, handler: this.index });
+    this.addRoute({ path: '/', method: HttpMethod.Post, handler: this.create });
+  }
+
+  public async index(_req: Request, res: Response) {
+    const offers = await this.offerService.find();
+    this.ok(res, fillDTO(OfferRdo, offers));
+  }
+
+  public async create(
+    { body }: Request<Record<string, unknown>, Record<string, unknown>, CreateOfferDto>,
+    res: Response
+  ): Promise<void> {
+    const result = await this.offerService.create(body);
+    this.created(res, fillDTO(OfferRdo, result));
+  }
+}
---
+++ src/modules/offer/rdo/offer.rdo.ts
@@ -0,0 +1,62 @@
+import { Expose, Type } from 'class-transformer';
+import UserRdo from '../../user/rdo/user.rdo.js';
+
+export default class OfferRdo {
+  @Expose()
+  public id!: string;
+
+  @Expose()
+  public title!: string;
+
+  @Expose()
+  public description!: string;
+
+  @Expose()
+  public postDate!: string;
+
+  @Expose()
+  public city!: string;
+
+  @Expose()
+  public previewImage!: string;
+
+  @Expose()
+  public images!: string;
+
+  @Expose()
+  public isPremium!: boolean;
+
+  @Expose()
+  public isFavorite!: boolean;
+
+  @Expose()
+  public rating!: number;
+
+  @Expose()
+  public type!: string;
+
+  @Expose()
+  public bedroomsNumber!: number;
+
+  @Expose()
+  public maxAdultsNumber!: number;
+
+  @Expose()
+  public price!: number;
+
+  @Expose()
+  public features!: string;
+
+  @Expose({ name: 'host'})
+  @Type(() => UserRdo)
+  public user!: UserRdo;
+
+  @Expose()
+  public commentCount!: number;
+
+  @Expose()
+  public latitude!: number;
+
+  @Expose()
+  public longitude!: number;
+}
